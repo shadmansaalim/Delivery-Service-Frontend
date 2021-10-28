91ffd5bc83ef8880040c41ee2e5551f78377170b
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-29 00:53:50 +0600

    Carousel implemeted with images

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import img1 from '../../../images/carousel1.jpeg'
+import img2 from '../../../images/carousel2.jpeg'
+import img3 from '../../../images/carousel3.jpeg'
 
 const Banner = () => {
     return (
-        <div>
+        <Carousel fade>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img2}
+                    alt="Second slide"
+                />
 
-        </div>
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
