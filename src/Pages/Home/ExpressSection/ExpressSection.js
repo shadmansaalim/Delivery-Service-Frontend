69f7f56a4a593da31da2ca8b1b4d6ad74ba0c8c0
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-30 20:40:06 +0600

    Footer Updated

--- src/Pages/Home/ExpressSection/ExpressSection.js
+++ src/Pages/Home/ExpressSection/ExpressSection.js
@@ -5,7 +5,7 @@ const ExpressSection = () => {
         <section className="row d-flex align-items-center" style={{ marginTop: '100px', marginBottom: '100px' }}>
 
             <img className="col-lg-5 mx-auto" src="https://www.zoom2u.com.au/wp-content/themes/zoom2u2018/images/map2.png" alt="" />
-            <div className="col-lg-6">
+            <div className="col-lg-6 mt-3 mt-lg-0">
                 <h3 className="mb-4" style={{ color: '#0057A7' }}>Seamless Express Delivery Service</h3>
                 <p>Delivering parcels has never been easier and more personalised. Zoom2u’s technology makes sure that your package gets to where it needs to be and how it has to be.</p>
                 <p>Zoom2u enables you to keep your eyes on your parcel while it’s on route to its destination. Our technology allows you to track your parcel in real time, call the courier when you feel the need to and get informative updates directly on your mobile device. It’s easy.</p>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -77,97 +77,11 @@ const Footer = () => {
 
                 <section className="mb-4">
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-                        repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-                        eum harum corrupti dicta, aliquam sequi voluptate quas.
+                        A Full Stack Order Delivery Application built using ReactJs, React Bootstrap, NodeJs, ExpressJs and for storing data MongoDB. This is a simple application where users can send any item to their relatives and friends from anywhere.
                     </p>
                 </section>
 
 
-
-                <section className="">
-
-                    <div className="row">
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5 >Couriers & Freight</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Parcel & Courier Services</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Domestic Freight</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Import Services</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Track & Trace</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5>eCommerce</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Online Self Service</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Regular Senders</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Import Services</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Latest Services</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5>Logistics Solutions</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Industry Solutions</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Corporate Solutions</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Project Management</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Mailbox Rental Services</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                            <h5>Baggage & Removals</h5>
-
-                            <ul className="list-unstyled mb-0">
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Fragile & Sensitive Goods</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Personal Goods</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Baggage Removals</a>
-                                </li>
-                                <li>
-                                    <a href="#!" className="text-white text-decoration-none">Excess Baggage</a>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>
-
-                </section>
 
             </div>
 
