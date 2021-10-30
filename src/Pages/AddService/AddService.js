58353925110cd7bf8f0e06d3d17e6f75f8e8bcf4
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-10-30 22:08:29 +0600

    New Service Add Functionality implemeted

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 
 const AddService = () => {
+    // const [service, setService] = useState(null);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        fetch('http://localhost:5000/addService', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.insertedId) {
+                    alert('Service Addded')
+                }
+            })
+    }
+
+
     return (
-        <div>
-            
+        <div className="vh-100">
+            <div className="col-lg-6 mx-auto shadow-lg p-3 rounded-3 mt-5">
+                <div className="card-body p-md-5 mx-md-4">
+
+                    <div className="text-center">
+                        <img src="https://www.packsend.com.au/assets/img/pas-logo.svg" style={{ width: '185px' }} alt="logo" />
+                        <p className="mt-3 mb-4 pb-1">We don't deliver boxes we deliver happiness</p>
+                    </div>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <p>Add a new service</p>
+
+                        <div className="form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingServiceName" placeholder="name@example.com" required  {...register("name")} />
+                            <label htmlFor="floatingServiceName">Service Name</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingServiceId" placeholder="name@example.com" required {...register("sID")} />
+                            <label htmlFor="floatingServiceId">Service ID</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <textarea class="form-control" placeholder="Leave a comment here" id="floatingServiceDescription" style={{ height: '100px' }} {...register("description")}></textarea>
+                            <label htmlFor="floatingServiceDescription">Description</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingServiceImage" placeholder="name@example.com" required {...register("img")} />
+                            <label htmlFor="floatingServiceImage">Image Link</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="number" className="form-control" id="floatingServicePrice" placeholder="name@example.com" required {...register("price")} />
+                            <label htmlFor="floatingServicePrice">Price (AUD)</label>
+                        </div>
+                        <div className="text-center mt-4">
+                            <button className="btn app-blue-btn text-white btn-block mb-3" type="submit">Continue</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/ManageOrders/ManageOrders.js
+++ src/Pages/ManageOrders/ManageOrders.js
@@ -9,43 +9,43 @@ const ManageOrders = () => {
 
     useEffect(() => {
         fetch('http://localhost:5000/allOrders')
-        .then(res => res.json())
-        .then(data => setAllOrders(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setAllOrders(data));
+    }, [])
 
-    const handleDeleteOrder = (id,name) => {
+    const handleDeleteOrder = (id, name) => {
         swal({
             title: "Are you sure?",
             text: `Order of ${name} will be cancelled!`,
             icon: "warning",
             buttons: true,
             dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-                const url = `http://localhost:5000/orders/${id}`;
-                fetch(url, {
-                    method: 'DELETE'
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        if (data.deletedCount > 0) {
-                            const remainingOrders = allOrders.filter(order => order._id !== id);
-                            setAllOrders(remainingOrders);
-                    
-                            
-                        }
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    const url = `http://localhost:5000/orders/${id}`;
+                    fetch(url, {
+                        method: 'DELETE'
                     })
-                swal("Order Cancelled Successfully", {
-                    icon: "success",
-                });
-              
-            }
-          });
-        
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.deletedCount > 0) {
+                                const remainingOrders = allOrders.filter(order => order._id !== id);
+                                setAllOrders(remainingOrders);
+
+
+                            }
+                        })
+                    swal("Order Cancelled Successfully", {
+                        icon: "success",
+                    });
+
+                }
+            });
+
     }
 
-    const updateOrderStatus = (e,id) => {
+    const updateOrderStatus = (e, id) => {
         const updatedStatus = e.target.value;
         console.log(updatedStatus);
         swal({
@@ -54,83 +54,83 @@ const ManageOrders = () => {
             icon: "warning",
             buttons: true,
             dangerMode: true,
-          })
-          .then((willUpdate) => {
-            if (willUpdate) {
-                const url = `http://localhost:5000/ordersUpdate/${id}`;
-        fetch(url, {
-            method: "PUT",
-            headers: {
-                "content-type": "application/json"
-                
-            },
-            body: JSON.stringify([updatedStatus])
         })
-            .then(res => res.json())
-            .then(data => {
-                if (data.modifiedCount > 0) {
-                    swal("Order Updated Successfully", {
-                        icon: "success",
-                    });
+            .then((willUpdate) => {
+                if (willUpdate) {
+                    const url = `http://localhost:5000/ordersUpdate/${id}`;
+                    fetch(url, {
+                        method: "PUT",
+                        headers: {
+                            "content-type": "application/json"
+
+                        },
+                        body: JSON.stringify([updatedStatus])
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.modifiedCount > 0) {
+                                swal("Order Updated Successfully", {
+                                    icon: "success",
+                                });
+                            }
+                        })
+
                 }
-            })
-               
-            }
-          });
+            });
     }
     return (
-       <div class="container mx-auto table-responsive text-nowrap">
-           {
-               allOrders.length
-               ?
-               <table class="table table-striped" style={{marginBottom: '120px', marginTop: '120px'}}>
-               <thead>
-                 <tr>
-                   <th>ID</th>
-                   <th>Sender</th>
-                   <th>Sender Address</th>
-                   <th>Receiver</th>
-                   <th>Receiver Address</th>
-                   <th>Service</th>
-                   <th>Status</th>
-                   <th>Actions</th>
-                 </tr>
-               </thead>
-               <tbody>
-                   {
-                       allOrders.map(order => 
-                         <tr>
-                           <th scope="row">{order._id}</th>
-                           <td>{order.senderName}</td>
-                           <td>{order.senderAddress}</td>
-                           <td>{order.receiverName}</td>
-                           <td>{order.receiverAddress}</td>
-                           <td>{order.serviceTaken}</td>
-                           <td>
-                              <select class="form-select mb-3 bg-warning" aria-label=".example" onChange={(e) => {
-                                  updateOrderStatus(e,order._id);
-                                         }}>
-                                 <option selected>{order.status}</option>
-                                 <option value="Accepted">Accepted</option>
-                                 <option value="Collected">Collected</option>
-                                 <option value="Shipped">Shipped</option>
-                                 <option value="Delivered">Delivered</option>
-                             </select>
-                           </td>
-                           <td>
-                           <button onClick={() => handleDeleteOrder(order._id, order.senderName)} class="btn btn-sm btn-outline-danger">Cancel Order</button>
-                           </td>
-                         </tr>
-                       )
-                   }
-               </tbody>
-             </table>
-             :
-             <section className="text-center vh-100 d-flex justify-content-center align-items-center">
-            <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </section>
-           }
-       </div>
+        <div class="container mx-auto table-responsive text-nowrap vh-100 mt-5">
+            {
+                allOrders.length
+                    ?
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Sender</th>
+                                <th>Sender Address</th>
+                                <th>Receiver</th>
+                                <th>Receiver Address</th>
+                                <th>Service</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                allOrders.map(order =>
+                                    <tr>
+                                        <th scope="row">{order._id}</th>
+                                        <td>{order.senderName}</td>
+                                        <td>{order.senderAddress}</td>
+                                        <td>{order.receiverName}</td>
+                                        <td>{order.receiverAddress}</td>
+                                        <td>{order.serviceTaken}</td>
+                                        <td>
+                                            <select class="form-select mb-3 bg-warning" aria-label=".example" onChange={(e) => {
+                                                updateOrderStatus(e, order._id);
+                                            }}>
+                                                <option selected>{order.status}</option>
+                                                <option value="Accepted">Accepted</option>
+                                                <option value="Collected">Collected</option>
+                                                <option value="Shipped">Shipped</option>
+                                                <option value="Delivered">Delivered</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <button onClick={() => handleDeleteOrder(order._id, order.senderName)} class="btn btn-sm btn-outline-danger">Cancel Order</button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                    :
+                    <section className="text-center vh-100 d-flex justify-content-center align-items-center">
+                        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    </section>
+            }
+        </div>
 
 
     );
